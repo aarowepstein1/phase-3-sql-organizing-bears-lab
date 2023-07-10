d3518dbfd21ec601630877ba16e5e6ecd8e80cd8
Aarow Epstein <aarowepstein1@gmail.com>
2023-07-09 17:29:51 -0700

    commit tree 278da59e2863313eda3c776c8d9c1abde825c96f

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Mr. Chocolate", 7, "M", "brown", "aggressive", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Rowdy", 6, "M", "black", "calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Tabitha", 5, "F", "white", "calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Sergeant Brown", 3, "F", "brown", "aggressive", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Melissa", 13, "F", "brown", "calm", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Grinch", 4, "M", "black", "calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES("Wendy", 2, "F", "white", "calm", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES(NULL, 5, "F", "brown", "aggressive", 1);
